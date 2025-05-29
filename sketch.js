a381b875c36ee2567a04a85f7467c864ad882eab
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-05-29 17:01:39 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,27 +8,21 @@ function preload() {
 // Verifying Structure
 function setup() {
   noCanvas();
-  if (table) {
-    print('Rows:', table.getRowCount());
-    print('Columns:', table.getColumnCount());
-    print('Column Headers:', table.columns);
-  } else {
-    print('Error: Table not loaded');
+  
+  if (!table) {
+    print("Table not loaded");
+    return;
   }
-}
+
+  print('Rows:', table.getRowCount());
+  print('Columns:', table.getColumnCount());
+  print('Column Headers:', table.columns);
 
 // Accessing Attributes
 for (let i = 0; i < table.getRowCount(); i++) {
-  let name = table.getString(i, 'Name');
-  let activity = table.getString(i, 'Agrivoltaic Activities');
-  let systemSize = table.getString(i, 'System Size');
-  let siteSize = table.getString(i, 'Site Size');
-  let year = table.getString(i, 'Year Installed');
-  let tech = table.getString(i, 'PV Technology');
-  let arrayType = table.getString(i, 'Type Of Array');
-  let habitat = table.getString(i, 'Habitat Type');
-  let crops = table.getString(i, 'Crop Types');
-  let animals = table.getString(i, 'Animal Type');
-
-  print(`${i}: ${name}, ${activity}, ${systemSize}, ${siteSize}, ${year}, ${tech}, ${arrayType}, ${habitat}, ${crops}, ${animals}`);
+    let name = table.getString(i, 'Name');
+    let activity = table.getString(i, 'Agrivoltaic Activities');
+    // Access other fields...
+    print(`${name} | ${activity}`);
+  }
 }
