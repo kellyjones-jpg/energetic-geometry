19b9204479dc9dea76f3212d5d21d3a74808b67d
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-05-30 14:59:53 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,15 +8,35 @@ function preload() {
 // Verifying Structure
 function setup() {
   noCanvas();
-  
-  if (!table) {
-    print("Table not loaded");
-    return;
-  }
 
-  print('Rows:', table.getRowCount());
-  print('Columns:', table.getColumnCount());
-  print('Column Headers:', table.columns);
+  for (let i = 0; i < table.getRowCount(); i++) {
+    let name = table.getString(i, 'Name');
+    let activity = table.getString(i, 'Agrivoltaic Activities');
+    let systemSize = table.getString(i, 'System Size');
+    let siteSize = table.getString(i, 'Site Size');
+    let year = table.getString(i, 'Year Installed');
+    let tech = table.getString(i, 'PV Technology');
+    let arrayType = table.getString(i, 'Type Of Array');
+    let habitat = table.getString(i, 'Habitat Type');
+    let crops = table.getString(i, 'Crop Types');
+    let animals = table.getString(i, 'Animal Type');
+
+    let siteInfo = `
+      <strong>${name}</strong><br>
+      Activities: ${activity}<br>
+      System Size: ${systemSize}<br>
+      Site Size: ${siteSize}<br>
+      Year Installed: ${year}<br>
+      PV Tech: ${tech}<br>
+      Array Type: ${arrayType}<br>
+      Habitat: ${habitat}<br>
+      Crops: ${crops}<br>
+      Animals: ${animals}<br><hr>
+    `;
+
+    createDiv(siteInfo);
+  }
+}
 
 // Accessing Attributes
 for (let i = 0; i < table.getRowCount(); i++) {
