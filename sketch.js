b01adaa1deb7b8054c5022ef895cac9e99730f3c
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-06-22 16:51:57 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -152,10 +152,12 @@ function centerSlider() {
 }
 
 function draw() {
-  // Draw background image
+  // Draw background image dimmed and semi-transparent
+  tint(150, 200);  // soften colors, 200 alpha opacity
   image(bgImg, 0, 0, width, height);
+  tint(255);       // reset tint to default
   
-  fill(0);
+  fill(255);
   textSize(24);
   textAlign(CENTER, TOP);
   text("Year: " + selectedYear, width / 2, 30);
