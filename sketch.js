8ead1903d45790243629155809368012953735d1
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-06-29 11:40:02 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -261,10 +261,6 @@ function draw() {
   // Draw background image dimmed and semi-transparent
   image(bgImg, 0, 0, width, height);
   noStroke();
-  rectMode(CORNER);          // switch to CORNER mode to cover full canvas
-  fill(0, 180);              // black with semi-transparent alpha
-  rect(0, 0, width, height); // cover entire canvas
-  rectMode(CENTER);          // restore if needed for other rects
   
   fill(255);
   textSize(24);
