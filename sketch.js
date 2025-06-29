ee10fcf99d2b0af0f86ab3e2743f5c7a79bfeb91
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-06-29 12:50:15 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -262,7 +262,7 @@ function draw() {
   image(bgImg, 0, 0, width, height);
   noStroke();
   rectMode(CORNER);          // switch to CORNER mode to cover full canvas
-  fill(0, 100);              // black with semi-transparent alpha
+  fill(0, 130);              // black with semi-transparent alpha
   rect(0, 0, width, height); // cover entire canvas
   rectMode(CENTER);          // restore if needed for other rects
   
