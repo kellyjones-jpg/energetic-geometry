187cd475a1f5ef7c57eff4074071d6491edd5ef3
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-07-02 15:33:00 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -201,7 +201,7 @@ function setup() {
   let numCols = floor((initialWidth - padding) / (shapeSize + padding));
   numCols = max(numCols, 1);
   let numRows = ceil(yearEntries.length / numCols);
-  let fixedHeight = 885;
+  let fixedHeight = 895;
   cnv = createCanvas(initialWidth, fixedHeight);
   cnv.parent('sketch-container');
 
