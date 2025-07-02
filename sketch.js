8cb88ed072ebae83ddb56463c0777232ec6c80ff
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-07-02 16:19:58 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -989,7 +989,7 @@ function drawArrayOverlay(arrayType, activities, x, y, size, strokeW = 1.2, dens
   pop();
 }
 
-function drawCrosshatchGridMultiColor(activities, size, density = 9) {
+function drawCrosshatchGridMultiColor(activities, size, density = 5) {
   let colorCount = activities.length;
 
   push();
