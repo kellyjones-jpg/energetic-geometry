961f199dbc53f15a46ec8fa2a97a011334abd1d1
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-07-07 14:29:33 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1283,9 +1283,9 @@ function updateCounters(yearEntries) {
     if (!isNaN(entry.acres)) totalAcres += entry.acres;
   }
 
-  const siteCounter = new CountUp('site-count', siteCount, { duration: 1 });
-  const mwCounter = new CountUp('megawatt-count', totalMegawatts, { duration: 1, decimalPlaces: 1, useGrouping: true });
-  const acreCounter = new CountUp('acre-count', totalAcres, { duration: 1, decimalPlaces: 1, useGrouping: true });
+  const siteCounter = new countUp.CountUp('site-count', siteCount, { duration: 1 });
+  const megawattCounter = new countUp.CountUp('megawatt-count', megawattCount, { duration: 1 });
+  const acreCounter = new countUp.CountUp('acre-count', acreCount, { duration: 1 });
 
   if (!siteCounter.error) siteCounter.start();
   if (!mwCounter.error) mwCounter.start();
