0ed151a597f5aa59d5c4dcba49cba1c984dae3d4
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-07-09 09:09:11 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -177,13 +177,6 @@ const combinedIcon = `
   </svg>
 `;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const aboutLink = document.getElementById("about-link");
-  if (aboutLink) {
-    aboutLink.insertAdjacentHTML("beforeend", combinedIcon);
-  }
-});
-
 function preload() {
   table = loadTable('data/inspire-agrivoltaics-20250702.csv', 'csv', 'header');
   bgImg = loadImage('images/pexels-tomfisk-19117245.jpg');
@@ -360,11 +353,11 @@ function draw() {
  // === MINIMAL SUPREMATIST YEAR LABEL ===
     let centerX = width / 2;
     let labelY = 40;
-    let yearY = labelY + 38;
+    let yearY = labelY + 40;
 
     // Label: smaller
     textFont('Helvetica');
-    textSize(30);
+    textSize(28);
     textAlign(CENTER, BOTTOM);
     fill(255);
     text("Year Installed:", centerX, labelY);
