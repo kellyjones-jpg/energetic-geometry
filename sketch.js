c4e11b30e9775900e449c114b3d271da5ceda605
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-07-15 11:49:03 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1387,12 +1387,3 @@ function drawPVWarpStyle(pvType, activities, x, y, size) {
 
   pop();
 }
-
-// Allow hyperlink inside tooltip to open despite p5.js event interference
-document.addEventListener('click', (event) => {
-  const anchor = event.target.closest('a.hyperlink-tooltip');
-  if (anchor) {
-    event.stopPropagation(); // stop p5 mousePressed from hijacking
-    window.open(anchor.href, '_blank', 'noopener');
-  }
-});
