d415ca9e8ff28b017f0f5dd86eaf2bb4da8b2bf6
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-07-21 14:46:46 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1232,7 +1232,7 @@ function drawSuprematistOpShadowRect(baseSize, systemSize, habitat = [], posX, p
   pg.rectMode(CENTER);
 
   // Draw shape to buffer
-  drawShapeByTypeTo(pg, shapeType, glowW, glowH);
+  drawShapeByTypeTwo(pg, shapeType, glowW, glowH);
   pg.pop();
 
   // Blur and render
@@ -1296,7 +1296,36 @@ function drawSuprematistOpShadowRect(baseSize, systemSize, habitat = [], posX, p
   };
 }
 
-function drawShapeByType(pg, type, w, h) {
+function drawShapeByType(type, w, h) {
+  switch (type) {
+    case 'hexagon':
+      beginShape();
+      for (let i = 0; i < 6; i++) {
+        let angle = TWO_PI / 6 * i - PI / 2;
+        vertex(cos(angle) * w / 2, sin(angle) * h / 2);
+      }
+      endShape(CLOSE);
+      break;
+    case 'ellipse':
+      ellipse(0, 0, w, h);
+      break;
+    case 'rect':
+      rect(0, 0, w, h);
+      break;
+    case 'diamond':
+      push();
+      rotate(PI / 4); // Rotate square 45 degrees to get diamond
+      rect(0, 0, w, h);
+      pop();
+      break;
+    case 'square':
+    default:
+      rect(0, 0, w, h);
+      break;
+  }
+}
+
+function drawShapeByTypeTwo(pg, type, w, h) {
   switch (type) {
     case 'ellipse':
       pg.ellipse(0, 0, w, h);
