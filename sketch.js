99949a2a96ea604b50fd9d984c0849ae5e161c4d
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-08-08 12:06:24 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -770,7 +770,7 @@ function draw() {
    if (modalPreviewEntry && modalPreviewCallback) {
    const entry = modalPreviewEntry;
 
-   const scaleFactor = 0.5;
+   const scaleFactor = 0.75;
    const pg = createGraphics(200, 200);
    pg.pixelDensity(1);
    pg.clear();
