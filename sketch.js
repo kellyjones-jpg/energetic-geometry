8a271fecfcb4be4cd0a5a1ece13aceedd1b9329c
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-08-09 13:36:02 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -509,7 +509,7 @@ function renderEntryVisual(entry, pg, isModal = false) {
 
   const size = 140;
   const strokeW = 3;
-  const scaleFactor = 0.95;
+  const scaleFactor = 1.15;
   const offset = size * (scaleFactor - 1) / 2;
 
   pg.translate(offset, offset);
@@ -792,6 +792,7 @@ pop(); // end entry group
 if (modalPreviewEntry && modalPreviewCallback) {
   const entry = modalPreviewEntry;
 
+  const scaleFactor = 0.45;
   const pg = createGraphics(200, 200);
   pg.pixelDensity(1);
   pg.clear();
@@ -800,6 +801,8 @@ if (modalPreviewEntry && modalPreviewCallback) {
   pg.ellipseMode(CENTER);
 
   pg.push();
+  pg.translate(pg.width / 2, pg.height / 2);
+  pg.scale(scaleFactor);
 
   renderEntryVisual(entry, pg);
 
