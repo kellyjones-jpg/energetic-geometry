1aadb02e94b21bf76174cf6341c9b8fcd768d1d0
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2025-08-11 14:54:04 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -582,7 +582,7 @@ function renderEntryVisual(entry, pg, isModal = false) {
 
    const size = 140;
    const strokeW = 3;
-   const scaleFactor = 1.15;
+   const scaleFactor = 1.2;
    const offset = size * (scaleFactor - 1) / 2;
 
    pg.translate(offset, offset);
